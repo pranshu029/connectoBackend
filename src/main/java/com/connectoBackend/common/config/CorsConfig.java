0bf5b096aee6f45dcc808d453fe30411a68eb3d1
pranshu029 <pranshud2005@gmail.com>
2026-08-23 15:54:47 +0530

    latest

--- src/main/java/com/connectoBackend/common/config/CorsConfig.java
+++ src/main/java/com/connectoBackend/common/config/CorsConfig.java
@@ -15,54 +15,12 @@ public class CorsConfig {
         @Value("${application.cors.allowed-origins}")
         private List<String> allowedOrigins;
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//
-//        CorsConfiguration configuration = new CorsConfiguration();
-//
-//        configuration.setAllowedOrigins(allowedOrigins);
-//
-//        // -> Allowed HTTP methods
-//        configuration.setAllowedMethods(
-//                List.of(
-//                        "GET",
-//                        "POST",
-//                        "PUT",
-//                        "PATCH",
-//                        "DELETE",
-//                        "OPTIONS"
-//                )
-//        );
-//
-//        // -> Allow all request headers
-//        configuration.setAllowedHeaders(List.of("*"));
-//
-//        // -> Allow cookies/authentication credentials
-//        configuration.setAllowCredentials(true);
-//
-//        // -> Expose Authorization header if needed by frontend
-//        configuration.setExposedHeaders(
-//                List.of("Authorization")
-//        );
-//
-//        UrlBasedCorsConfigurationSource source =
-//                new UrlBasedCorsConfigurationSource();
-//
-//        // -> Apply CORS configuration to all API endpoints
-//        source.registerCorsConfiguration("/**", configuration);
-//
-//        return source;
-//    }
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        // -> Allow Vercel frontend deployments
-        configuration.setAllowedOriginPatterns(
-                List.of("https://*.vercel.app")
-        );
+        configuration.setAllowedOrigins(allowedOrigins);
 
         // -> Allowed HTTP methods
         configuration.setAllowedMethods(
@@ -82,7 +40,7 @@ public class CorsConfig {
         // -> Allow cookies/authentication credentials
         configuration.setAllowCredentials(true);
 
-        // -> Expose Authorization header
+        // -> Expose Authorization header if needed by frontend
         configuration.setExposedHeaders(
                 List.of("Authorization")
         );
@@ -90,8 +48,51 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
+        // -> Apply CORS configuration to all API endpoints
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
     }
+
+    // bean for vercel
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        // -> Allow Vercel frontend deployments
+//        configuration.setAllowedOriginPatterns(
+//                List.of("https://*.vercel.app")
+//        );
+//
+//        // -> Allowed HTTP methods
+//        configuration.setAllowedMethods(
+//                List.of(
+//                        "GET",
+//                        "POST",
+//                        "PUT",
+//                        "PATCH",
+//                        "DELETE",
+//                        "OPTIONS"
+//                )
+//        );
+//
+//        // -> Allow all request headers
+//        configuration.setAllowedHeaders(List.of("*"));
+//
+//        // -> Allow cookies/authentication credentials
+//        configuration.setAllowCredentials(true);
+//
+//        // -> Expose Authorization header
+//        configuration.setExposedHeaders(
+//                List.of("Authorization")
+//        );
+//
+//        UrlBasedCorsConfigurationSource source =
+//                new UrlBasedCorsConfigurationSource();
+//
+//        source.registerCorsConfiguration("/**", configuration);
+//
+//        return source;
+//    }
 }
